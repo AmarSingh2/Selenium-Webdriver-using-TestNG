1398dd7d730ba1814926be1074ad79b7e8db2e57
AmarSingh2 <114733083+AmarSingh2@users.noreply.github.com>
2024-01-24 22:24:46 +0530

    Add files via upload

---
+++ HDFCBankDetails.java
@@ -0,0 +1,17 @@
+package com.selenium.TestNG;
+
+import org.testng.annotations.Test;
+
+public class HDFCBankDetails {
+
+	@Test
+	public void AccountDetail_01() {
+		System.out.println("The HDFC account detail 01");
+	}
+	
+	@Test
+	public void AccountDetail_02() {
+		System.out.println("The HDFC account detail 02");
+	}
+
+}
---
+++ ICICIBankDetails.java
@@ -0,0 +1,16 @@
+package com.selenium.TestNG;
+
+import org.testng.annotations.Test;
+
+public class ICICIBankDetails {
+
+	@Test
+	public void AccountDetails_01() {
+		System.out.println("The ICICI account detail 01");
+	}
+	
+	@Test
+	public void AccountDetails_02() {
+		System.out.println("The ICICI account details 02");
+	}
+}
